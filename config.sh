afdec5f219bbac266627819f066bb30fc271d25e
Karl Mantle <130184376+karl-mantle@users.noreply.github.com>
2026-08-04 15:46:49 +0100

    feat: .wslconfig

--- config.sh
+++ config.sh
@@ -5,5 +5,5 @@ GIT_NAME="Karl Mantle"
 GIT_EMAIL="130184376+karl-mantle@users.noreply.github.com"
 
 # SSH settings
-SSH_KEY_NAME="github_ed25519"
+SSH_KEY_NAME="id_ed25519"
 SSH_KEY_PATH="$HOME/.ssh/$SSH_KEY_NAME"
